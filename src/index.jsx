c04c6eef93614f23cf62e7e70ff576e475a8f7c1
Peter Kim <petersangkyukim@gmail.com>
2025-04-27 20:28:56 -0400

    myflix update

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,22 @@
-import { createRoot } from "react-dom/client";
-import { MainView } from "./components/main-view/main-view";
+import React from 'react'; // Ensure React is imported for JSX
+import { createRoot } from 'react-dom/client'; // Import ReactDOM for rendering
+import { MainView } from './Components/MainView/main-view'; // Import your main view
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import './index.scss'; // Import the SCSS styles
+import Container from 'react-bootstrap/Container'; // Import the Container component
 
-import "./index.scss";
-
-const App = () => {
- return <MainView />;
+// Main component (will eventually use all the others)
+const MyFlixApplication = () => {
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
-const container = document.querySelector("#root");
-const root = createRoot(container);
-root.render(<App />);
+// Finds the root of your app
+const container = document.querySelector("#root"); // Ensure this element exists in your HTML
+const root = createRoot(container); // Create root to use in rendering
+
+// Tells React to render your app in the root DOM element
+root.render(<MyFlixApplication />);
