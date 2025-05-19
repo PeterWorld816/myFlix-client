9675c41785c49267ef54f46c8d27783b4397634c
Peter Kim <petersangkyukim@gmail.com>
2025-05-18 23:53:16 -0400

    update for 3.4

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,31 +1,39 @@
 // src/components/movie-card/movie-card.jsx
 import React from 'react';
-// Here you import the PropTypes library
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { Button, Card } from "react-bootstrap";
 
 
-// The BookCard function component 
-export const MovieCard = ({ movie, onMovieClick }) => {
-  console.log(movie);
+export function MovieCard({ movie, onClick }) {
   return (
-      <Card className="h-100">
-          <Card.Img variant="top" src={movie.imagePath} className="w-100" />
-          <Card.Body>
-              <Card.Title>{movie.title}</Card.Title>
-              <Card.Text>{movie.director.name}</Card.Text>
-              <Button onClick={() => onMovieClick(movie)} variant="link">
-                  Open
-              </Button>
-          </Card.Body>
-      </Card>
+    <Card className="h-100">
+      <Card.Img
+        variant="top"
+        src={movie.ImagePath}
+        alt={`${movie.Title} poster`}
+        className="w-100"
+      />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director?.Name}</Card.Text>
+
+        <Button variant="link" onClick={() => onClick(movie)}>
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
-};
+}
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string,
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+    }),
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
 };
-
-
